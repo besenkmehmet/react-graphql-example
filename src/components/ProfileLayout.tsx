74a35199b4e0e3ae6c08714a7622ad4c9bf084f9
besenkmehmet <besenkmehmet@gmail.com>
2022-11-27 00:24:44 +0300

    feature: side profile added

--- src/components/ProfileLayout.tsx
+++ src/components/ProfileLayout.tsx
@@ -1,7 +1,24 @@
+import ProfileInformation from './ProfileInformation';
 import styles from './ProfileLayout.module.css';
 
 function ProfileLayout() {
-  return <div className={styles.layoutGrid}></div>;
+  return (
+    <div className={styles.layoutGrid}>
+      <ProfileInformation
+        user={{
+          login: 'Test',
+          avatarUrl:
+            'https://avatars.githubusercontent.com/u/62817155?s=96&v=4',
+          followers: {
+            totalCount: 50,
+          },
+          following: {
+            totalCount: 100,
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default ProfileLayout;
