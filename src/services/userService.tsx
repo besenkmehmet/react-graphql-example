e06361d5c78eb85531dec9e406e7bd133e7af390
besenkmehmet <besenkmehmet@gmail.com>
2022-11-27 00:43:20 +0300

    feat: fetch user repositories service

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -1,6 +1,7 @@
 import User from '../interfaces/user';
 import apolloClient from '../plugins/apollo';
 import { gql } from '@apollo/client';
+import Repository from '../interfaces/repository';
 
 export const userService = {
   fetchUsers: async (query: string) => {
@@ -27,5 +28,48 @@ export const userService = {
         users = result.data.search.nodes;
       });
     return users;
+  },
+  fetchUserRepositories: async (userName: string, query: string) => {
+    let repositories: Array<Repository> = [];
+    await apolloClient
+      .query({
+        query: gql`
+            {
+                search(query: "user:${userName} ${query}", type: REPOSITORY, first: 10) {
+                    pageInfo {
+                        endCursor
+                        startCursor
+                        hasPreviousPage
+                        hasNextPage
+                    }
+                    nodes {
+                    ... on Repository {
+                        name
+                        description
+                        forkCount
+                        licenseInfo {
+                        id
+                        name
+                        }
+                        updatedAt
+                        languages(first: 1) {
+                        edges {
+                            node {
+                            name
+                            color
+                            }
+                        }
+                        }
+                    }
+                    }
+                }
+            }
+        `,
+      })
+      .then((result) => {
+        const searchResults = result.data.search;
+        repositories = searchResults.nodes;
+      });
+    return repositories;
   },
 };
