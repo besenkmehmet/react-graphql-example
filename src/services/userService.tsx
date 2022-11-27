e5c658e60196e5cb257776b5a23623d7b950788f
besenkmehmet <besenkmehmet@gmail.com>
2022-11-27 18:23:12 +0300

    docs: add user service documentation

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -4,6 +4,11 @@ import { gql } from '@apollo/client';
 import Repository from '../interfaces/repository';
 
 export const userService = {
+  /**
+   * Fetch all users with a given query
+   * @param  {String} query Query used to search the users
+   * @return {Array<User>} Return all the fetched users
+   */
   fetchUsers: async (query: string) => {
     let users: Array<User> = [];
     if (!query) return users;
@@ -30,6 +35,12 @@ export const userService = {
       });
     return users;
   },
+  /**
+   * Fetch all repositories of a user with a given query
+   * @param  {String} userName User to be searched through
+   * @param  {String} query Query used to search the repositories
+   * @return {Array<Repository>} Return all the fetched repositories
+   */
   fetchUserRepositories: async (userName: string, query: string) => {
     let repositories: Array<Repository> = [];
     await apolloClient
@@ -73,6 +84,11 @@ export const userService = {
       });
     return repositories;
   },
+  /**
+   * Fetch info of a user
+   * @param  {String} userName User to be searched
+   * @return {User} Return all the info of the user
+   */
   fetchUserByLogin: async (userName: string) => {
     let user: User = {
       login: userName,
